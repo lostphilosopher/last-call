691a9f71a182dbc5850e9866f4ce0541550a974b
lostphilosopher <wandersen02@gmail.com>
2015-06-30 15:21:03 -0500

    Restructuing assets and vendor-esque

---
+++ assets/javascript/utility.js
@@ -0,0 +1,9 @@
+/*
+ * Utility JS methods (non-prject specific)
+ */
+
+// Show / Hide Demo
+$( "#utility-js-demo-button" ).click(function() {
+  alert('FOO');
+  $( "#utility-js-demo-target" ).toggle();
+});
