9516356baaed0ecf1ecd2972a7fde593971223a8
lostphilosopher <wandersen02@gmail.com>
2015-10-10 16:06:57 -0500

    Fixing JS

--- assets/javascript/utility.js
+++ assets/javascript/utility.js
@@ -3,7 +3,8 @@
  */
 
 // Show / Hide Demo
-$( "#utility-js-demo-button" ).click(function() {
-  alert('FOO');
-  $( "#utility-js-demo-target" ).toggle();
+$( document ).ready(function() {
+  $( "#utility-js-demo-button" ).click(function() {
+    $( "#utility-js-demo-target" ).toggle();
+  });
 });
